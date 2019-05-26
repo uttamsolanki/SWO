d2e7bfb2bb6cd87adc82c00a89ae742dbd49ae1d
Uttam Solanki <kanudo@Uttams-MacBook-Pro.local>
2019-05-26 00:37:03 -0400

    change Url

--- src/app/AppConstants.ts
+++ src/app/AppConstants.ts
@@ -1,5 +1,5 @@
 export class AppConstants {
-  //public static get baseURL(): string { return "https://vast-gorge-98517.herokuapp.com/"; }
-  public static get baseURL(): string { return "http://localhost:8042/"; }
+  public static get baseURL(): string { return "https://vast-gorge-98517.herokuapp.com/"; }
+  //public static get baseURL(): string { return "http://localhost:8042/"; }
 }
 //"https://vast-gorge-98517.herokuapp.com/"
