79c03eb7389fd7e19417fd4bcc9fdf3162276559
rheamac <rheamac999@gmail.com>
2019-07-25 22:24:46 -0400

    added icon images to info button

--- src/app/report/create/create.component.ts
+++ src/app/report/create/create.component.ts
@@ -1356,13 +1356,15 @@ export class CreateComponent implements OnInit {
     for (let rs in refs) {
       tempHtml+=refs[rs]+'<br>';
     }
-    if(Object.keys(data.range.ref).length !== 0 ){
-      let refr = data.range.ref;
-      var rrHtml="<hr><h6><b>Rs:</b></h6>";
-      for (let rr in refr) {
-        rrHtml+=refr[rr]+'<br>';
+    if(data.range.ref) {
+      if (Object.keys(data.range.ref).length !== 0) {
+        let refr = data.range.ref;
+        var rrHtml = "<hr><h6><b>Rs:</b></h6>";
+        for (let rr in refr) {
+          rrHtml += refr[rr] + '<br>';
+        }
+        tempHtml = tempHtml + rrHtml;
       }
-      tempHtml=tempHtml+rrHtml;
     }
 
   //  console.log(data.hasOwnProperty("ref"));
