9508f5237ef4531c6ae7cc33da66e82ac9d7f2df
Uttam Solanki <uttamsolanki.2@gmail.com>
2020-09-20 19:29:52 -0400

    change URL

--- src/app/AppConstants.ts
+++ src/app/AppConstants.ts
@@ -1,7 +1,7 @@
 export class AppConstants {
   //public static get baseURL(): string { return "/"; }
-  // public static get baseURL(): string { return "http://localhost:8042/"; }
-  public static get baseURL(): string { return "https://vast-gorge-98517.herokuapp.com/"; }
+   public static get baseURL(): string { return "http://localhost:8042/"; }
+  //public static get baseURL(): string { return "https://vast-gorge-98517.herokuapp.com/"; }
 }
 //"https://vast-gorge-98517.herokuapp.com/"
 
