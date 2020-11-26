de04f597b9089ef4f9f9d0295b468a516168eab1
Uttam Solanki <uttamsolanki.2@gmail.com>
2020-11-26 09:40:47 -0500

    change URL

--- src/app/AppConstants.ts
+++ src/app/AppConstants.ts
@@ -1,7 +1,7 @@
 export class AppConstants {
- // public static get baseURL(): string { return "/"; }
+  public static get baseURL(): string { return "/"; }
   // public static get baseURL(): string { return "http://localhost:8042/"; }
-   public static get baseURL(): string { return "https://calculateghg.ca/"; }
+  // public static get baseURL(): string { return "https://calculateghg.ca/"; }
 }
 //"https://vast-gorge-98517.herokuapp.com/"
 
